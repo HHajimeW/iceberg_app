a4f732e84dcec1b0a657fa6c82a7fae36670ab33
HHajimeW <h-watanabe@ohtsuki.ics.keio.ac.jp>
2019-08-28 10:05:32 +0900

    first commit

---
+++ src/main/kotlin/com/example/iceberg_app/IcebergAppApplication.kt
@@ -0,0 +1,11 @@
+package com.example.iceberg_app
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class IcebergAppApplication
+
+fun main(args: Array<String>) {
+    runApplication<IcebergAppApplication>(*args)
+}
